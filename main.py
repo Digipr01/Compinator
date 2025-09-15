7772c30d5386376cd3a469432ff6a8c8efd0c91b
Digipr01 <tychokruithof2008@hotmail.com>
2025-09-15 22:25:40 +0200

    Attempted to process data, but errors. Have to stop for now am fixing later

--- main.py
+++ main.py
@@ -15,31 +15,51 @@ sheetId= os.getenv("SHEET_ID")
 
 succes = False
 week = 0
+events = []
+peopleDict = {}
 
 def getData(week):
     URL = f"https://sheets.googleapis.com/v4/spreadsheets/{sheetId}/values/Weekly%20Comp%20{week}!A1:Z?alt=json&key={key}"
     response = requests.get(URL)
-    print(response.status_code)
+    print("Response code:", response.status_code)
     responseData = dict(response.json())
     return responseData['values']
 
-def sortData(dataDict):
+def checkEvents(dataDict):
     def listData(data):
         list = []
         for i in data:
             list.append(i)
         return list
     dataList = listData(dataDict)
-    print(dataList, "\n", dataList[0])
+
     events = []
     for i in dataList[0]:
-        print(i)
         if i != "Tijdstempel" and i != "Discord username" and i != "Have you filmed your solves and do you want to show off? Drop the YouTube link here!":
-            # if not in list, 
             events.append(i.split(" ")[0])
-    print(events)
+    print("Events:", events)
+    return events, dataList
 
-
+def processData(events, data):
+    def assignPersonalSolves(listitem, events):
+        for i in range(len(listitem)):
+            if i <= 1:
+                continue
+            print("check2", listitem[1], type(listitem[0]), len(listitem[i]), listitem[i], peopleDict.get(listitem[1]))
+            #try:
+            if peopleDict.get(listitem[1]) != None:
+                peopleDict[str(listitem[1])][str(events[i])].append(listitem[i])
+            else:
+                peopleDict[str(listitem[1])].update(events[i], list(listitem[i])) # keyerror
+            print(peopleDict)
+            #except:
+                #print("Error has occured")
+            print("Check", peopleDict, type(peopleDict))
+    for i in data:
+        if i == data[0]:
+            continue
+        assignPersonalSolves(i, events)
+        
 while succes == False:
     prevWeek = week
     while week == prevWeek:
@@ -50,6 +70,8 @@ while succes == False:
             print("Not a valid week number! Try again")
             continue
     data = getData(week)
-    sortData(data)
+    events, datalist = checkEvents(data)
     succes = True
+    processData(events, datalist)
+
 print("Succesfully executed program!")
