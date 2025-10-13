37923fa2ff95d3570cb467c4d0f3683d03213879
Digipr01 <tychokruithof2008@hotmail.com>
2025-10-13 19:37:13 +0200

    made a few changes

--- main.py
+++ main.py
@@ -50,19 +50,38 @@ def processData(events, data):
                 continue
             elif i >= defaultLength:
                 break
+                #fmc vanishes
+            print(3, peopleDict.get(listitem[1]))
             if peopleDict.get(listitem[1]) != None:
                 if peopleDict[str(listitem[1])].get(events[i-2]) == None:
                     peopleDict[str(listitem[1])][events[i-2]] = []
                 peopleDict[str(listitem[1])][events[i-2]].append(listitem[i])
             else:
                 peopleDict[str(listitem[1])][events[i-2]] = [listitem[i]]
+    
+    def calculateAverages(person, events):
+        def convertToFloat(timelist):
+            print(timelist)
+        
+        eventsHad = []
+        print(person)
+        for event in events:
+            if event in eventsHad:
+                continue
+            eventsHad.append(event)
+            convertToFloat(peopleDict[str(person)].get(str(event)))
+    
     for i in data:
         if i == data[0]:
             continue
         assignPersonalSolves(i, events)
+        print(i)
+    for person in peopleDict:
+        print(3, peopleDict.get(person))
+        calculateAverages(person, events)
         
 while succes == False:
-    prevWeek = week
+    prevWeek = week 
     while week == prevWeek:
         tempweek = input("What is the new weekly number? #")
         try: 
@@ -71,9 +90,12 @@ while succes == False:
             print("Not a valid week number! Try again")
             continue
     data = getData(week)
+    print(1, data)
     events, datalist, defaultLength = checkEvents(data)
-    succes = True
+    print(2, data, defaultLength)
     processData(events, datalist)
     print(peopleDict)
+    
+    succes = True
 
 print("Succesfully executed program!")
