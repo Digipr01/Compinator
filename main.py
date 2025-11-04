0861938c04786d390119652d23f0bfe523dc6cc4
Digipr01 <tychokruithof2008@hotmail.com>
2025-11-04 19:28:41 +0100

    Back to the basics, Gonna rewrite all processing code

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from dotenv import load_dotenv
-from collections import defaultdict
 import os
 import requests
-
-cacheFolder = "/Cache"
-dataFolder = "/Data"
+from colorama import Fore, Style
 
 load_dotenv()
 
@@ -12,18 +9,25 @@ key = os.getenv("API_KEY")
 mail = os.getenv("SERVICE_EMAIL")
 sheetId= os.getenv("SHEET_ID")
 
-# don't forget to update USB .env file when adding more variables
+#How to print in color: 
+#print(f"{Fore.RED}{2}{Style.RESET_ALL}")
 
 succes = False
 week = 0
 events = []
-peopleDict = defaultdict(dict)
 defaultLength = 0
+shortEvents = []
+
+# future attempt to process data
+class Person:
+    def __init__(self, name, eventslist):
+        self.name = name
+        self.events = eventslist
 
 def getData(week):
     URL = f"https://sheets.googleapis.com/v4/spreadsheets/{sheetId}/values/Weekly%20Comp%20{week}!A1:Z?alt=json&key={key}"
     response = requests.get(URL)
-    print("Response code:", response.status_code)
+    print(f"{Fore.GREEN if response.status_code == 200 else Fore.RED}Response code:, {response.status_code}{Style.RESET_ALL}")
     responseData = dict(response.json())
     return responseData['values']
 
@@ -35,67 +39,67 @@ def checkEvents(dataDict):
         return list
     dataList = listData(dataDict)
 
-    events = []
+    events, shortEvents = [], []
     for i in dataList[0]:
         if i != "Tijdstempel" and i != "Discord username" and i != "Have you filmed your solves and do you want to show off? Drop the YouTube link here!":
-            events.append(i.split(" ")[0])
+            events.append(i.split(" ")[0].lower())
+            if i not in shortEvents:
+                shortEvents.append(i.split(" ")[0].lower())
             
-    print("Events:", events)
-    return events, dataList, len(events)
+    print(f"{Fore.BLUE}Events: {shortEvents}{Style.RESET_ALL}")
+    return dataList, len(events)
 
-def processData(events, data):
-    def assignPersonalSolves(listitem, events):
-        for i in range(len(listitem)):
-            if i <= 1:
-                continue
-            elif i >= defaultLength:
-                break
-                #fmc vanishes
-            print(3, peopleDict.get(listitem[1]))
-            if peopleDict.get(listitem[1]) != None:
-                if peopleDict[str(listitem[1])].get(events[i-2]) == None:
-                    peopleDict[str(listitem[1])][events[i-2]] = []
-                peopleDict[str(listitem[1])][events[i-2]].append(listitem[i])
-            else:
-                peopleDict[str(listitem[1])][events[i-2]] = [listitem[i]]
+# def processData(events, data):
+#     def assignPersonalSolves(listitem, events):
+#         for i in range(len(listitem)):
+#             if i <= 1:
+#                 continue
+#             elif i >= defaultLength:
+#                 break
+#                 #fmc vanishes
+#             print(3, peopleDict.get(listitem[1]))
+#             if peopleDict.get(listitem[1]) != None:
+#                 if peopleDict[str(listitem[1])].get(events[i-2]) == None:
+#                     peopleDict[str(listitem[1])][events[i-2]] = []
+#                 peopleDict[str(listitem[1])][events[i-2]].append(listitem[i])
+#             else:
+#                 peopleDict[str(listitem[1])][events[i-2]] = [listitem[i]]
     
-    def calculateAverages(person, events):
-        def convertToFloat(timelist):
-            print(timelist)
+#     def calculateAverages(person, events):
+#         def convertToFloat(timelist):
+#             print(timelist)
         
-        eventsHad = []
-        print(person)
-        for event in events:
-            if event in eventsHad:
-                continue
-            eventsHad.append(event)
-            convertToFloat(peopleDict[str(person)].get(str(event)))
+#         eventsHad = []
+#         print(person)
+#         for event in events:
+#             if event in eventsHad:
+#                 continue
+#             eventsHad.append(event)
+#             convertToFloat(peopleDict[str(person)].get(str(event)))
     
-    for i in data:
-        if i == data[0]:
-            continue
-        assignPersonalSolves(i, events)
-        print(i)
-    for person in peopleDict:
-        print(3, peopleDict.get(person))
-        calculateAverages(person, events)
+#     for i in data:
+#         if i == data[0]:
+#             continue
+#         assignPersonalSolves(i, events)
+#         print(i)
+#     for person in peopleDict:
+#         print(3, peopleDict.get(person))
+#         calculateAverages(person, events)
         
-while succes == False:
-    prevWeek = week 
-    while week == prevWeek:
+while succes == False: 
+    while True:
         tempweek = input("What is the new weekly number? #")
         try: 
             week = int(tempweek)
         except:
             print("Not a valid week number! Try again")
             continue
+        break
     data = getData(week)
-    print(1, data)
-    events, datalist, defaultLength = checkEvents(data)
-    print(2, data, defaultLength)
-    processData(events, datalist)
-    print(peopleDict)
-    
+    # print(1, data)
+    datalist, defaultLength = checkEvents(data)
+    # print(2, data, defaultLength)
+    # processData(events, datalist)
     succes = True
 
-print("Succesfully executed program!")
+print(f"{Fore.GREEN}Succesfully executed program!{Style.RESET_ALL}")
